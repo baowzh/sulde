d3e33d785f5caeddef8ea1af369414b9d45e97fe
baowzh <imubwz@126.com>
2014-10-09 22:57:37 +0800

    tian jia hangye leibie !

---
+++ src/com/mongolia/website/model/ProfessionValue.java
@@ -0,0 +1,27 @@
+package com.mongolia.website.model;
+/**
+ * 职业类别
+ * @author Administrator
+ *
+ */
+public class ProfessionValue {
+	private String professioncode;
+	private String professionname;
+
+	public String getProfessioncode() {
+		return professioncode;
+	}
+
+	public void setProfessioncode(String professioncode) {
+		this.professioncode = professioncode;
+	}
+
+	public String getProfessionname() {
+		return professionname;
+	}
+
+	public void setProfessionname(String professionname) {
+		this.professionname = professionname;
+	}
+
+}
